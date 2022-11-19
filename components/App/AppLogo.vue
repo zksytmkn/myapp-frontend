61c5f3e0df0f42d434c8a3bd5abd4d9d49a0a715
zksytmkn <zksytmkn4869@gmail.com>
2022-11-19 10:58:23 +0900

    Add Home Toolbar

---
+++ components/App/AppLogo.vue
@@ -0,0 +1,25 @@
+<template>
+  <v-avatar
+    color="black"
+    size="34"
+    class="my-app-log"
+  >
+    <span
+      class="white--text text-subtitle-2"
+    >
+      Edi
+    </span>
+  </v-avatar>
+</template>
+
+<script>
+export default {
+}
+</script>
+
+<style lang="scss" scoped>
+.my-app-log {
+  margin-right: 8px;
+  cursor: pointer;
+}
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,37 @@
 <template>
   <v-app>
-    <home-app-bar />
+    <home-app-bar
+      :menus="menus"
+    />
+
+    <v-img
+      dark
+      src="https://picsum.photos/id/11/1920/1080?blur=1"
+      gradient="to top right, rgba(19,84,122,.3), rgba(128,208,199,.3)"
+      :height="imgHeight"
+    >
+      <v-row
+        align="center"
+        justify="center"
+        :style="{ height: `${imgHeight}px` }"
+      >
+        <v-col
+          cols="12"
+          class="text-center"
+        >
+          <h1 class="display-1 mb-4">
+            持続可能な社会の為に。
+          </h1>
+          <h4
+            class="subheading"
+            :style="{ letterSpacing: '5px' }"
+          >
+            -廃棄される食べものとそれらを必要とする消費者を繋ぐサービス-
+          </h4>
+        </v-col>
+      </v-row>
+    </v-img>
+
     <v-sheet>
       <v-container
         fluid
@@ -10,6 +41,26 @@
           v-for="(menu, i) in menus"
           :key="`menu-${i}`"
         >
+
+          <v-col
+            cols="12"
+          >
+            <v-card
+              flat
+            >
+              <v-card-title
+                class="justify-center display-1"
+              >
+                {{ $t(`menus.${menu.title}`) }}
+              </v-card-title>
+              <v-card-text
+                class="text-center"
+              >
+                {{ menu.subtitle }}
+              </v-card-text>
+            </v-card>
+          </v-col>
+
           <v-col
             cols="12"
           >
@@ -41,6 +92,7 @@ export default {
   },
   data () {
     return {
+      imgHeight: 500,
       menus: [
         { title: 'about', subtitle: 'このサイトはブログ"独学プログラマ"で公開されているチュートリアルのデモアプリケーションです' },
         { title: 'products', subtitle: '他にはない優れた機能の数々' },
