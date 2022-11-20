f48cf0b1e31d0a4a10de24f1c51ba7e333796580
zksytmkn <zksytmkn4869@gmail.com>
2022-11-20 18:26:30 +0900

    Add user form components

--- pages/signup.vue
+++ pages/signup.vue
@@ -1,7 +1,25 @@
 <template>
-  <div>
-    signup.vue
-  </div>
+  <user-form-card>
+    <template
+      #user-form-card-content
+    >
+    <v-form
+      v-model="isValid"
+    >
+      <user-form-name />
+      <user-form-email />
+      <user-form-password />
+      <v-btn
+        :disabled="!isValid"
+        block
+        color="appblue"
+        class="white--text"
+      >
+        登録する
+      </v-btn>
+    </v-form>
+    </template>
+  </user-form-card>  
 </template>
 
 <script>
