86d3853df9865f094defc5655a76550aa4d87a42
zksytmkn <zksytmkn4869@gmail.com>
2022-12-02 02:52:53 +0900

    Add middleware for silent refresh

---
+++ middleware/silent-refresh-token.js
@@ -0,0 +1,22 @@
+export default async ({ $auth, $axios, store, route, redirect, isDev }) => {
+  if ($auth.isExistUserAndExpired()) {
+    if (isDev) {
+      console.log('Execute silent refresh!!')
+    }
+    await $axios.$post('/api/v1/auth_token/refresh')
+      .then(response => $auth.login(response))
+      .catch(() => {
+        const msg = 'セッションの有効期限が切れました。' +
+                    'もう一度ログインしてください'
+        // TODO test
+        console.log(msg)
+        // TODO トースター出力
+        // store.dispatch('getToast', { msg })
+        // TODO アクセスルート記憶
+        // store.dispatch('getRememberPath', route)
+        // Vuexの初期化(セッションはサーバで削除済み)
+        $auth.resetVuex()
+        return redirect('/login')
+      })
+  }
+}
