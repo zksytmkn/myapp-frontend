9a13ab477fcf1ada40b78ce3a04423cb15eefd7d
zksytmkn <zksytmkn4869@gmail.com>
2022-12-02 03:31:21 +0900

    Add middleware for authentication

---
+++ middleware/authentication.js
@@ -0,0 +1,23 @@
+export default async ({ $auth, store, route, redirect }) => {
+  // リダイレクトを必要としないパス
+  const notRedirectPaths = ['account', 'project']
+  if (notRedirectPaths.includes(route.name)) {
+    return false
+  }
+
+  // ログイン前ユーザー処理
+  if (!$auth.loggedIn()) {
+    // ユーザー以外の値が存在する可能性があるので全てを削除する
+    await $auth.logout()
+
+    const msg = 'まずはログインしてください'
+    const color = 'info'
+    // TODO test
+    console.log(msg, color)
+    // TODO トースター出力
+    // store.dispatch('getToast', { msg, color })
+    // TODO アクセスルート記憶
+    // store.dispatch('getRememberPath', route)
+    return redirect('/login')
+  }
+}
